0305127f5597e3b0576ffc6ded47c2c59341c2f7
alonso130r <131227801+alonso130r@users.noreply.github.com>
2024-05-30 13:52:49 -0400

    finish blocks

---
+++ include/blockchain.h
@@ -0,0 +1,22 @@
+//
+// Created by Vijay Goyal on 2024-05-30.
+//
+
+#ifndef BLOCKCHAIN_CPP_BLOCKCHAIN_H
+#define BLOCKCHAIN_CPP_BLOCKCHAIN_H
+
+#include <vector>
+#include <block.h>
+
+class Blockchain {
+public:
+    Blockchain();
+
+    void addBlock(const Block &newBlock);
+    void printChain() const;
+
+private:
+    std::vector<Block> chain;
+};
+
+#endif //BLOCKCHAIN_CPP_BLOCKCHAIN_H
--- src/block.cpp
+++ src/block.cpp
@@ -3,11 +3,39 @@
 //
 
 #include "block.h"
+#include "sha256.h"
 #include <iostream>
-#include <ctime>
+#include <sstream>
 
 Block::Block(int idx, const std::string &data) : index(idx), data(data), timestamp(std::time(nullptr)), previousHash(""), hash(calculateHash()) {}
 
 std::string Block::calculateHash() const {
-    // add custom sha256
+    std::stringstream ss;
+    ss << index << data << timestamp << previousHash;
+
+    std::string input = ss.str();
+    unsigned char hash[32];
+    SHA256_CTX ctx;
+    sha256_init(&ctx);
+    sha256_update(&ctx, reinterpret_cast<const unsigned char*>(input.c_str()), input.size());
+    sha256_final(&ctx, hash);
+
+    std::stringstream hashString;
+    for (int i = 0; i < 32; ++i) {
+        hashString << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+    }
+
+    return hashString.str();
+}
+
+std::string Block::getHash() const {
+    return hash;
+}
+
+std::string Block::getPreviousHash() const {
+    return previousHash;
+}
+
+void Block::setPreviousHash(const std::string &prevHash) {
+    previousHash = prevHash;
 }
