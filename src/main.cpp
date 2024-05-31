689faba27318af8debd2c42eb64b007230dda2b8
alonso130r <131227801+alonso130r@users.noreply.github.com>
2024-05-31 11:39:06 -0400

    test program

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Vijay Goyal on 2024-05-31.
+//
+
+#include <iostream>
+#include "blockchain.h"
+
+int main() {
+    Blockchain bChain = Blockchain();
+
+    bChain.addBlock(Block(1, "Block 1 Data"));
+    bChain.addBlock(Block(2, "Block 2 Data"));
+
+    bChain.printChain();
+
+    return 0;
+}
